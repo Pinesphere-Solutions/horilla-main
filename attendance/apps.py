20ffc676615a02fd3e85edbe977e75a800007b5b
athithyag <athithyag24@gmail.com>
2026-01-05 16:13:37 +0530

    Updated frontend files

--- attendance/apps.py
+++ attendance/apps.py
@@ -1,5 +1,5 @@
 """
-This module defines the configuration for the 'attendance' app within the Horilla HRMS project.
+This module defines the configuration for the 'attendance' app within the Pinesphere HRMS project.
 """
 
 from django.apps import AppConfig
